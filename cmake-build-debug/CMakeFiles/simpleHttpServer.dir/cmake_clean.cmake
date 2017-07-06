5975e25ac85e3d5b5343c7ebf429b8cfde93960a
Pablo <pasanchez@uvigo.es>
2017-07-06 11:30:13 +0200

    fixed executable name

---
+++ cmake-build-debug/CMakeFiles/simpleHttpServer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simpleHttpServer.dir/main.cpp.o"
+  "CMakeFiles/simpleHttpServer.dir/Server.cpp.o"
+  "simpleHttpServer.pdb"
+  "simpleHttpServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simpleHttpServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
